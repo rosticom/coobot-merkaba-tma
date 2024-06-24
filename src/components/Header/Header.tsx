fb34bb28c74d7e8d791943d58889af62cb9e75f0
Rost G. <rostislav@Rostyslavs-Mac-mini.local>
2024-06-24 22:51:39 +0300

    wallet position 2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,12 @@ import './header.scss';
 
 export const Header = () => {
     return <header>
-        <div id="wallet-connect-button-wrapper">
-            <TonConnectButton />
-        </div>
+        {/* <div id="wallet-connect-button-wrapper"> */}
+        {/* <TonConnectButton /> */}
+        {/* </div> */}
         {/* <TonConnectButton /> */}
         {/* <span>My App with React UI</span> */}
         <span>Coobot</span>
+        <TonConnectButton />
     </header>
 }
