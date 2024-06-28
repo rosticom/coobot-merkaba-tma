d5153e41772ee85738524509d52c123c1fdca9b5
Rost G. <ros10@meta.ua>
2024-06-28 23:12:13 +0300

    fixed chart

--- src/components/TonusdChart/TonusdChart.tsx
+++ src/components/TonusdChart/TonusdChart.tsx
@@ -2,19 +2,21 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title} from 'chart.js';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+// ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title);
 
 const TonUsdChart = () => {
   const [chartData, setChartData] = useState({
     labels: [],
     datasets: [
-      {
+      { 
         label: 'TON/USD',
         data: [],
         // borderColor: 'rgb(75, 192, 192)',
-        borderColor: 'rgb(255, 255, 255)',
+        // borderColor: 'rgb(255, 255, 255)',
+        borderColor: 'rgb(102,170,238)',
         tension: 0.1
       }
     ]
@@ -23,12 +25,14 @@ const TonUsdChart = () => {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://api.coingecko.com/api/v3/coins/the-open-network/market_chart?vs_currency=usd&days=30'
+        'https://api.coingecko.com/api/v3/coins/the-open-network/market_chart?vs_currency=usd&days=120'
       );
       
       const prices = result.data.prices;
-      const labels = prices.map((price: unknown[]) => moment(price[0]).format('DD/MM/YYYY'));
-      const data = prices.map((price: unknown[]) => price[1]);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const labels = prices.map((price: any[]) => moment(price[0]).format('DD/MM/YYYY'));
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const data = prices.map((price: any[]) => price[1]);
 
       setChartData({
         labels: labels,
@@ -48,11 +52,11 @@ const TonUsdChart = () => {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        // position: 'top',
       },
       title: {
-        display: false,
-        text: 'TON/USD Exchange Rate (Last 30 Days)',
+        display: true,
+        text: 'TON/USD Exchange Rate (Last 120 Days)',
       },
     },
   };
