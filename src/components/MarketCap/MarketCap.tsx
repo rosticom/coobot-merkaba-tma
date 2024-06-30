e2588155678b7561d7eeabb271b206987650cd2f
Rost G. <ros10@meta.ua>
2024-06-30 03:31:19 +0300

    max cap label

--- src/components/MarketCap/MarketCap.tsx
+++ src/components/MarketCap/MarketCap.tsx
@@ -21,22 +21,22 @@ const MarketCap = () => {
   };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const saveData = async (newData: any) => {
-    try {
-      await axios.post(`https://api.telegram.org/bot${botToken}/setMyCommands`, {
-        commands: [{ command: 'data', description: JSON.stringify(newData) }]
-      });
-      setData(newData);
-    } catch (error) {
-      console.error('Error saving data:', error);
-    }
-  };
+  // const saveData = async (newData: any) => {
+  //   try {
+  //     await axios.post(`https://api.telegram.org/bot${botToken}/setMyCommands`, {
+  //       commands: [{ command: 'data', description: JSON.stringify(newData) }]
+  //     });
+  //     setData(newData);
+  //   } catch (error) {
+  //     console.error('Error saving data:', error);
+  //   }
+  // };
 
   return (
     <div>
       <h2>Max ⋅ {data ? JSON.stringify(data): '$3,666.69'}</h2>
       {/* {data ? <p>Saved data: {JSON.stringify(data)}</p> : <p>Loading...</p>} */}
-      <button onClick={() => saveData({ market_cap: '$3,666.69' })}>Save New Data</button>
+      {/* <button onClick={() => saveData({ market_cap: '$3,666.69' })}>Save New Data</button> */}
     </div>
   );
 };
